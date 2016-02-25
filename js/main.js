5d55dff4b3a6033bc623dd2200f8081bf2a9bd8e
Tommy Nguyen <quang.nnd.hcmus@gmail.com>
2016-02-25 22:39:55 +0700

    keyboard handle

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+function findNextTabStop(el) {
+	var universe = document.querySelectorAll('input, button, select, textarea, a[href], div[tabindex]');
+	var list = Array.prototype.filter.call(universe, function(item) {return item.tabIndex >= "0"});
+	var index = list.indexOf(el);
+	return list[index + 1] || list[0];
+}
+function findPreviousTabStop(el) {
+	var universe = document.querySelectorAll('input, button, select, textarea, a[href], div[tabindex]');
+	var list = Array.prototype.filter.call(universe, function(item) {return item.tabIndex >= "0"});
+	var index = list.indexOf(el);
+	return list[index - 1] || list[list-1];
+}
 $(document).ready(function() {
 	var carouselCount = 0;
     $(".owl-carousel").each(function () {
@@ -63,63 +75,6 @@ $(document).ready(function() {
 		});
         carouselCount++;
     });
-    var discover_owl = $('#discover-section');
-    discover_owl.owlCarousel({
-        items : 6,
-		lazyLoad : true,
-		navigation : false,
-		pagination : false,
-		addClassActive : true,
-		afterInit : function() {
-			//http://melmo.github.io/accessibility/berlin.js/code/dist/owlcarousel.html
-			//http://www.jpotapova.info/blog/accessibility-enhancing-owl-carousel-with-keyboard-navigation/
-			var owl = this;
-		    this.$owlItems.attr('aria-selected','false');
-		    //this.$elem.$('#owl-single-example .owl-item.active').attr('aria-selected','true'); // let screen readers know an item is active
-
-		    // add instructions to keyboard users that are only visible when the carousel is focused
-		    this.$elem.find(".owl-wrapper-outer").append('');
-
-		    // listen for keyboard input
-		    $(document).on('keydown', function(e){
-
-		        var $focusedElement = $(document.activeElement),
-		        singleOwl = owl,
-		            type = e.which == 39? 'next': null,
-		            type = e.which == 37? 'prev': type,
-		            type = e.which == 13? 'enter':type;
-
-		        // if the carousel is focused, use left and right arrow keys to navigate
-		        if($focusedElement.attr('id') === $(owl.$elem).get(0).id){
-
-		          if (type == 'next') {
-		            singleOwl.next();
-		          } else if (type == 'prev') {
-		            singleOwl.prev();
-		          }
-
-		        // if the prev and next buttons are focused, catch "Enter" and navigate in the right direction
-		        } else if (type == 'enter') {
-		        if ($focusedElement.hasClass('owl-next')) {
-		          singleOwl.next();
-		        } else if ($focusedElement.hasClass('owl-prev')) {
-		          singleOwl.prev();
-		        }
-		      }
-		  });
-		},
-		// let screen readers know which slide is active after navigation or reinit
-		afterAction : function() {
-		  this.$owlItems.attr('aria-selected','false');
-		}
-    });
-    $("#discover_next").click(function(){
-		discover_owl.trigger('owl.next');
-	});
-
-	$("#discover_previous").click(function(){
-		discover_owl.trigger('owl.prev');
-	});
     $("#menu_left ul li").click(function(){
     	var $this = $(this);
     	var index = $this.index();
@@ -131,4 +86,21 @@ $(document).ready(function() {
     	$this.addClass("active");
     });
     $("#menu_left ul li.active a").focus();
+    $(document).on('keydown', function(e){
+    	var activeNode = document.activeElement;
+    	switch(e.which){
+    		case 39:
+    		case 40:
+				findNextTabStop(activeNode).focus(); 
+    			break;
+			case 37:
+    		case 38:
+    			findPreviousTabStop(activeNode).focus();
+    			break;
+			case 13:
+				activeNode.click();
+				break;
+    	}
+		e.preventDefault();
+    });
 });
